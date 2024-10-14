01645825f04849e0a1dfd7a51ba42677f7fa8d7d
burcineren <burcin.ern969@gmail.com>
2024-10-15 00:51:54 +0300

    feat: reservation form

--- src/app/reservation-form/reservation-form.component.ts
+++ src/app/reservation-form/reservation-form.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
-
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, Validator } from '@angular/forms';
 @Component({
   selector: 'app-reservation-form',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './reservation-form.component.html',
   styleUrl: './reservation-form.component.scss'
 })
 export class ReservationFormComponent {
 
+  reservationForm : FormGroup = new FormGroup({
+    
+  });
 }
