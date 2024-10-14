d237cc8e3ba19b6cd421c46ae74839d6f6e78772
burcineren <burcin.ern969@gmail.com>
2024-10-15 00:29:44 +0300

    feat: app routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path: '',
+    loadComponent: () => import('./home/home.component').then(m => m.HomeComponent)
+},
+{
+    path: 'list',
+    loadComponent: () => import('./reservation-list/reservation-list.component').then(m => m.ReservationListComponent)
+},
+{
+    path: 'new',
+    loadComponent: () => import('./reservation-form/reservation-form.component').then(m => m.ReservationFormComponent)
+},
+];
