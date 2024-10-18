f09e6b63ef954daae371b3140a9e71e77c861c9d
burcineren <burcin.ern969@gmail.com>
2024-10-18 21:48:15 +0300

    feat: reservation service

--- src/app/reservation/reservation.service.ts
+++ src/app/reservation/reservation.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { Reservation } from '../models/reservation';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReservationService {
 
-  constructor() { }
+  private reservations: Reservation[] = [];
+
+  //CRUD
+
+  getReservations(): Reservation[] {
+    return this.reservations;
+  }
+  getReservation(id: string): Reservation | undefined{
+    return this.reservations.find(r => r.id === id);
+  }
+  addReservation(reservation: Reservation): void {
+    this.reservations.push(reservation);
+  }
+
+  deleteReservation(id: string): void {
+    let index = this.reservations.findIndex(r => r.id === id);
+    this.reservations.splice(index, 1);
+  }
+  updateReservation(updateReservation: Reservation): void {
+    let index = this.reservations.findIndex(r => r.id === updateReservation.id);
+    this.reservations[index] = updateReservation;
+  }
 }
