4970ae12e61b0cb3be34d669a5227ba951d544f1
burcineren <burcin.ern969@gmail.com>
2024-11-01 12:37:02 +0300

    feat: search-area

--- src/app/reservation-list/reservation-list.component.ts
+++ src/app/reservation-list/reservation-list.component.ts
@@ -3,15 +3,19 @@ import { Reservation } from '../models/reservation';
 import { ReservationService } from '../reservation/reservation.service';
 import { Router, RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-reservation-list',
   standalone: true,
-  imports: [RouterModule,HttpClientModule],
+  imports: [RouterModule, HttpClientModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule],
   templateUrl: './reservation-list.component.html',
   styleUrl: './reservation-list.component.scss'
 })
-export class ReservationListComponent implements OnInit  {
+export class ReservationListComponent implements OnInit {
 
   reservations: Reservation[] = [];
   private reservationService = inject(ReservationService);
@@ -31,7 +35,7 @@ export class ReservationListComponent implements OnInit  {
     });
   }
   deleteReservation(id: any) {
-    this.reservationService.deleteReservation(id).subscribe(()=> {
+    this.reservationService.deleteReservation(id).subscribe(() => {
       console.log("delete reservation successfully")
     });
   }
