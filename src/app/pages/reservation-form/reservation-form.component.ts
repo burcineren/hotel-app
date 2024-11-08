c51e8c2fb14fd7315c8b3daa79c78631ec10ee86
burcineren <burcin.ern969@gmail.com>
2024-11-09 01:42:59 +0300

    feat: resevation state entegration

--- src/app/pages/reservation-form/reservation-form.component.ts
+++ src/app/pages/reservation-form/reservation-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, inject,ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, inject, ChangeDetectionStrategy } from '@angular/core';
 import { FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { FormBuilder, FormGroup, Validator } from '@angular/forms';
 import { ReservationService } from '../../core/services/reservation/reservation.service';
@@ -7,8 +7,8 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { HomeComponent } from '../home/home.component';
 import { CalendarModule } from 'primeng/calendar';
 import { ButtonModule } from 'primeng/button';
-import {AddReservation} from "../../core/store/reservations/resevation.action";
-import {Store, select} from "@ngrx/store";
+import { AddReservation, LoadReservations } from "../../core/store/reservations/resevation.action";
+import { Store, select } from "@ngrx/store";
 import { Observable } from 'rxjs';
 import { AppState } from '../../app.state';
 
@@ -29,17 +29,22 @@ import { AppState } from '../../app.state';
 export class ReservationFormComponent implements OnInit {
   submitted = false;
   date: Date = new Date();
-   value = null;
+  value = null;
   private formBuilder = inject(FormBuilder);
   private reservationService = inject(ReservationService);
   private router = inject(Router);
   reservations$: Observable<Reservation[]>;
-  // private store = inject(Store<{reservations: Reservation[]}>);
+  isLoading$: Observable<boolean>;
   private activatedRoute = inject(ActivatedRoute);
-  constructor(private store :Store<AppState>){
-    this.reservations$= store.pipe(select('reservation'));
-    this.reservations$.subscribe(res => console.log(res))
-  } 
+  constructor(private store: Store<AppState>) {
+    this.reservations$ = this.store.select((state) => state.reservation.reservations)
+    this.isLoading$ = this.store.select(state => state.reservation.loading);
+    this.loadReservations();
+  }
+  loadReservations() {
+    this.store.dispatch(LoadReservations());
+  }
+
   reservationForm: FormGroup = new FormGroup({
     // checkInDate:new FormControl(''),
 
@@ -70,7 +75,6 @@ export class ReservationFormComponent implements OnInit {
     if (this.reservationForm.valid) {
       let reservation: Reservation = this.reservationForm.value;
       this.reservationService.addReservation(reservation);
-      // this.store.dispatch(AddReservation({id, checkInDate, checkOutDate, guestName, guestEmail, roomNumber}))
       let id = this.activatedRoute.snapshot.paramMap.get('id')
       if (id) {
         // update
@@ -82,10 +86,11 @@ export class ReservationFormComponent implements OnInit {
       }
       else {
         // new
-        this.store.dispatch(AddReservation(reservation));
-        this.reservationService.addReservation(reservation).subscribe(() => {
-          console.log("Create reservation processed")
-        })
+        this.store.dispatch(AddReservation({ reservation }));
+
+        // this.reservationService.addReservation(reservation).subscribe(() => {
+        //   console.log("Create reservation processed")
+        // })
         this.reservationForm.patchValue(reservation)
       }
       this.router.navigate(['/list']);
