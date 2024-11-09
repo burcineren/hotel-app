508867dc1caceab9f385273d0c587dbdf7152b1d
burcineren <burcin.ern969@gmail.com>
2024-11-09 23:05:06 +0300

    feat: dasboard template

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,40 +1,134 @@
-import { Component,ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
-import {KeyValuePipe, NgForOf} from '@angular/common';
-import {FormsModule} from '@angular/forms';
-import {RouterLink, RouterLinkActive} from '@angular/router';
+import { KeyValuePipe, NgForOf } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { SplitterModule } from 'primeng/splitter';
+import { SidebarModule } from 'primeng/sidebar';
+import { ButtonModule } from 'primeng/button';
+import { MenuItem } from 'primeng/api';
+import { BreadcrumbModule } from 'primeng/breadcrumb';
+import { PanelMenuModule } from 'primeng/panelmenu';
+import { InputIconModule } from 'primeng/inputicon';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [
-    RouterModule,
-    FormsModule,
-    KeyValuePipe,
-    NgForOf,
-    RouterLink,
-    RouterLinkActive,
-  ],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+    selector: 'app-home',
+    standalone: true,
+    imports: [
+        RouterModule,
+        FormsModule,
+        KeyValuePipe,
+        NgForOf,
+        RouterLink,
+        RouterLinkActive,
+        SplitterModule,
+        SidebarModule,
+        ButtonModule,
+        PanelMenuModule,
+        BreadcrumbModule,
+        InputIconModule, IconFieldModule, InputTextModule
+    ],
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.scss'
 })
-export class HomeComponent {
-  protected expanded = false;
-  protected open = false;
-  protected switch = false;
+export class HomeComponent implements OnInit {
+    sidebarVisible: boolean = false;
+    items: MenuItem[];
+    breadcrumbItem: MenuItem[] | undefined;
 
 
-  protected readonly drawer = {
-      Components: [
-          {name: 'Button', icon: 'bi bi-5-circle',link:'/list'},
-          {name: 'Input', icon: 'bi bi-5-circle',link:'/list'},
-          {name: 'Tooltip', icon: 'bi bi-5-circle',link:'/list'},
-      ],
-      Essentials: [
-          {name: 'Getting started', icon: 'bi bi-5-circle',link:'/list'},
-          {name: 'Showcase', icon: 'bi bi-5-circle',link:'/list'},
-          {name: 'Typography', icon: 'bi bi-5-circle',link:'/list'},
-      ],
-  };
+    home: MenuItem | undefined;
+
+    protected expanded = false;
+    protected open = false;
+    protected switch = false;
+    ngOnInit() {
+        this.breadcrumbItem = [
+            { label: 'Electronics' },
+            { label: 'Computer' },
+            { label: 'Accessories' }
+        ];
+        this.home = { icon: 'pi pi-home', routerLink: '/' };
+        this.items = [
+            {
+                label: 'Files',
+                icon: 'pi pi-file',
+                items: [
+                    {
+                        label: 'Documents',
+                        icon: 'pi pi-file',
+                        items: [
+                            {
+                                label: 'Invoices',
+                                icon: 'pi pi-file-pdf',
+                                items: [
+                                    {
+                                        label: 'Pending',
+                                        icon: 'pi pi-stop'
+                                    },
+                                    {
+                                        label: 'Paid',
+                                        icon: 'pi pi-check-circle'
+                                    }
+                                ]
+                            },
+                            {
+                                label: 'Clients',
+                                icon: 'pi pi-users'
+                            }
+                        ]
+                    },
+                    {
+                        label: 'Images',
+                        icon: 'pi pi-image',
+                        items: [
+                            {
+                                label: 'Logos',
+                                icon: 'pi pi-image'
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                label: 'Cloud',
+                icon: 'pi pi-cloud',
+                items: [
+                    {
+                        label: 'Upload',
+                        icon: 'pi pi-cloud-upload'
+                    },
+                    {
+                        label: 'Download',
+                        icon: 'pi pi-cloud-download'
+                    },
+                    {
+                        label: 'Sync',
+                        icon: 'pi pi-refresh'
+                    }
+                ]
+            },
+            {
+                label: 'Devices',
+                icon: 'pi pi-desktop',
+                items: [
+                    {
+                        label: 'Phone',
+                        icon: 'pi pi-mobile'
+                    },
+                    {
+                        label: 'Desktop',
+                        icon: 'pi pi-desktop'
+                    },
+                    {
+                        label: 'Tablet',
+                        icon: 'pi pi-tablet'
+                    }
+                ]
+            }
+        ];
+    }
 }
