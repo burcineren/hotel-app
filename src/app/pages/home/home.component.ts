0157d3edf68ef67bd3b37798149d385f60434a77
burcineren <burcin.ern969@gmail.com>
2024-11-16 22:00:50 +0300

    fix: css

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { KeyValuePipe, NgForOf } from '@angular/common';
@@ -13,6 +13,14 @@ import { PanelMenuModule } from 'primeng/panelmenu';
 import { InputIconModule } from 'primeng/inputicon';
 import { IconFieldModule } from 'primeng/iconfield';
 import { InputTextModule } from 'primeng/inputtext';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { ReservationListComponent } from '../reservation-list/reservation-list.component';
+import { ReservationFormComponent } from '../reservation-form/reservation-form.component';
 
 @Component({
     selector: 'app-home',
@@ -29,7 +37,13 @@ import { InputTextModule } from 'primeng/inputtext';
         ButtonModule,
         PanelMenuModule,
         BreadcrumbModule,
-        InputIconModule, IconFieldModule, InputTextModule
+        InputIconModule, IconFieldModule, InputTextModule,
+        MatIconModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    ReservationListComponent
     ],
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss'
@@ -38,19 +52,31 @@ export class HomeComponent implements OnInit {
     sidebarVisible: boolean = false;
     items: MenuItem[];
     breadcrumbItem: MenuItem[] | undefined;
-
+    selectedOption: any;
+    options = [
+      { label: 'Account', icon: 'pi pi-user' },
+      { label: 'Settings', icon: 'pi pi-cog' },
+      { label: 'Logout', icon: 'pi pi-sign-out' }
+    ];
 
     home: MenuItem | undefined;
 
+    constructor(private observer: BreakpointObserver) {}
     protected expanded = false;
     protected open = false;
     protected switch = false;
+    title = 'material-responsive-sidenav';
+    @ViewChild(MatSidenav)
+    sidenav!: MatSidenav;
+    isMobile= true;
+    isCollapsed = false;
     ngOnInit() {
         this.breadcrumbItem = [
             { label: 'Electronics' },
             { label: 'Computer' },
             { label: 'Accessories' }
         ];
+
         this.home = { icon: 'pi pi-home', routerLink: '/' };
         this.items = [
             {
@@ -130,5 +156,21 @@ export class HomeComponent implements OnInit {
                 ]
             }
         ];
+        this.observer.observe(['(max-width: 800px)']).subscribe((screenSize) => {
+            if(screenSize.matches){
+              this.isMobile = true;
+            } else {
+              this.isMobile = false;
+            }
+          });
     }
+    toggleMenu() {
+        if(this.isMobile){
+          this.sidenav.toggle();
+          this.isCollapsed = true;
+        } else {
+          this.sidenav.open();
+          this.isCollapsed = !this.isCollapsed;
+        }
+      }
 }
