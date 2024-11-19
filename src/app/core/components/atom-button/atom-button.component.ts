fe13510d549d68dec31a5a12cd46f58eec9af0b1
burcineren <burcin.ern969@gmail.com>
2024-11-20 00:51:58 +0300

    feat: atom button generated

--- src/app/core/components/atom-button/atom-button.component.ts
+++ src/app/core/components/atom-button/atom-button.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { NgClass, NgTemplateOutlet } from '@angular/common';
+import { Component,ChangeDetectionStrategy, computed, input, output, signal } from '@angular/core';
 
 @Component({
-  selector: 'app-atom-button',
+  selector: 'be-atom-button',
   standalone: true,
-  imports: [],
+  imports: [NgClass, NgTemplateOutlet,],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './atom-button.component.html',
-  styleUrl: './atom-button.component.scss'
 })
 export class AtomButtonComponent {
+  // iconComponent = contentChild(AtomIconComponent);
 
+  inlineFlex = input(true);
+
+  type = input('button');
+
+  classes = input('');
+
+  disabled = input<boolean>();
+
+  loading = input<boolean>();
+
+  mutableClasses = signal('');
+
+  onClick = output<MouseEvent>();
+
+  computedClasses = computed<{ [key: string]: boolean }>(() => {
+    const classes = this.mutableClasses() || this.classes();
+    return {
+      btn: true,
+      'btn-primary': !classes,
+      [classes]: !!classes,
+    };
+  });
 }
