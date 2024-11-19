1892dbf39ece42cd5aea1238beb7ff8aa4b7be4d
burcineren <burcin.ern969@gmail.com>
2024-11-19 22:44:37 +0300

    feat: orderview component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,25 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-import { CarouselModule } from 'primeng/carousel';
-import { ButtonModule } from 'primeng/button';
+
 import { TagModule } from 'primeng/tag';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { OverviewComponent } from './overview/overview.component';
+import { RoomCleanComponent } from './room-clean/room-clean.component';
 @Component({
     selector: 'app-home',
     standalone: true,
     imports: [
-        CarouselModule, ButtonModule,TagModule,
-        NgbModule
+        TagModule,
+        NgbModule,
+        OverviewComponent,
+        RoomCleanComponent,
     ],
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss'
 })
 export class HomeComponent {
 
-    images: { src: string; alt: string }[] = [
-        { src: 'assets/images/room-1.jpeg', alt: 'Slide 1' },
-        { src: 'assets/images/room-2.jpeg', alt: 'Slide 2' },
-        { src: 'assets/images/room-3.jpeg', alt: 'Slide 3' },
-        { src: 'assets/images/room-1.jpeg', alt: 'Slide 4' },
-        { src: 'assets/images/room-2.jpeg', alt: 'Slide 5' },
-      ];
+   
 }
--- src/app/pages/home/overview/overview.component.ts
+++ src/app/pages/home/overview/overview.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { CarouselModule } from 'primeng/carousel';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
-  selector: 'app-overview',
+  selector: 'overview',
   standalone: true,
-  imports: [],
+  imports: [
+    CarouselModule,
+    ButtonModule
+  ],
   templateUrl: './overview.component.html',
   styleUrl: './overview.component.scss'
 })
 export class OverviewComponent {
-
+  images: { src: string; alt: string }[] = [
+    { src: 'assets/images/room-1.jpeg', alt: 'Slide 1' },
+    { src: 'assets/images/room-2.jpeg', alt: 'Slide 2' },
+    { src: 'assets/images/room-3.jpeg', alt: 'Slide 3' },
+    { src: 'assets/images/room-1.jpeg', alt: 'Slide 4' },
+    { src: 'assets/images/room-2.jpeg', alt: 'Slide 5' },
+  ];
 }
