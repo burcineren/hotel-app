13a8fa90fbb05db255ea75d122ebd0cbcfe468ea
burcineren <burcin.ern969@gmail.com>
2024-11-24 17:22:01 +0300

    feat: atom loader component and loading service  implemented atom button

--- src/app/core/components/atom-button/atom-button.component.ts
+++ src/app/core/components/atom-button/atom-button.component.ts
@@ -1,28 +1,23 @@
-import { NgClass, NgTemplateOutlet } from '@angular/common';
-import { Component,ChangeDetectionStrategy, computed, input, output, signal } from '@angular/core';
+import {NgClass, NgTemplateOutlet} from '@angular/common';
+import {Component, ChangeDetectionStrategy, computed, input, output, signal, contentChild} from '@angular/core';
+import {AtomIconComponent} from "../svg-icons/atom-icon/atom-icon.component";
+import {AtomIconLoaderComponent} from "../svg-icons/atom-icon-loader";
 
 @Component({
   selector: 'be-atom-button',
   standalone: true,
-  imports: [NgClass, NgTemplateOutlet,],
+  imports: [NgClass, NgTemplateOutlet, AtomIconLoaderComponent],
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './atom-button.component.html',
 })
 export class AtomButtonComponent {
-  // iconComponent = contentChild(AtomIconComponent);
-
+  iconComponent = contentChild(AtomIconComponent);
   inlineFlex = input(true);
-
   type = input('button');
-
   classes = input('');
-
   disabled = input<boolean>();
-
   loading = input<boolean>();
-
   mutableClasses = signal('');
-
   onClick = output<MouseEvent>();
 
   computedClasses = computed<{ [key: string]: boolean }>(() => {
